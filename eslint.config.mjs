676288c203e53e9868313944dd885cf2761e7e18
rogerioweber <rogerioweber97@hotmail.com>
2026-07-06 08:55:49 -0300

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,83 @@
+import eslint from '@eslint/js'
+import { defineConfig } from 'eslint/config'
+import eslintPluginImport from 'eslint-plugin-import'
+import prettierPlugin from 'eslint-plugin-prettier'
+import tseslint from 'typescript-eslint'
+
+const config = defineConfig(
+  {
+    ignores: ['dist/**', 'node_modules/**', 'src/_experiments/**']
+  },
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ['*.js', '*.mjs', 'vitest.config.ts']
+        },
+        tsconfigRootDir: import.meta.dirname
+      },
+      sourceType: 'module',
+      globals: {
+        node: true
+      }
+    },
+
+    plugins: {
+      import: eslintPluginImport,
+      prettier: prettierPlugin
+    },
+
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', '.jsx', '.ts', '.tsx']
+        }
+      }
+    },
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+      ...eslintPluginImport.configs.recommended.rules,
+      ...eslintPluginImport.configs.typescript.rules,
+
+      '@typescript-eslint/no-extraneous-class': ['off'],
+      '@typescript-eslint/interface-name-prefix': 'off',
+      'import/no-absolute-path': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+      'lines-between-class-members': ['error', 'always'],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '_',
+          varsIgnorePattern: '_',
+          caughtErrorsIgnorePattern: '_'
+        }
+      ],
+      'import/no-unresolved': 'off',
+      'import/named': 'off',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index']
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true
+          }
+        }
+      ]
+    }
+  }
+)
+
+export default config
